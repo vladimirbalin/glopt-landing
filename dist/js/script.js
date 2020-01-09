b0ca091c7580f739a0b2349b11b8e3c9e1c701a0
pushka <pushkin@mail.ru>
2020-01-09 13:03:12 +0600

    before adapt

---
+++ dist/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	$('.reviews__slider').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev-arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/next-arrow.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+    //Клик на карту - карта активна, клик куда угодно, но не на карту - карта не активна
+    
+    document.addEventListener('click', function(e) {
+        let map = document.querySelector('#map-wrap iframe')
+        if(e.target.id === 'map-wrap') {
+          map.style.pointerEvents = 'all'
+        } else {
+          map.style.pointerEvents = 'none'
+        }
+      })
+
+    //pageup and  
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 800) {
+          $('.pageup').fadeIn();
+      } else {
+          $('.pageup').fadeOut();
+      }
+      
+  });
+  //Smooth scroll 
+  $("a[href^='#up']").click(function(){
+      var _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+  });
+});
