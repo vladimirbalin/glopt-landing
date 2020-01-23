5fc232fc6f6d6ee81cd7df2e94f26d2ce50bf6ea
VladimirB <vladimirzq@gmail.com>
2020-01-24 01:15:43 +0600

    imgfx

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,44 +1,47 @@
+"use strict";
 $(document).ready(function(){
 	$('.reviews__slider').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev-arrow.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/next-arrow.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    });
+		speed: 1200,
+		adaptiveHeight: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev-arrow.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/next-arrow.png"></button>',
+			
+	});
 
-    //Клик на карту - карта активна, клик куда угодно, но не на карту - карта не активна
-    
-    document.addEventListener('click', function(e) {
-        let map = document.querySelector('#map-wrap iframe')
-        if(e.target.id === 'map-wrap') {
-          map.style.pointerEvents = 'all'
-        } else {
-          map.style.pointerEvents = 'none'
-        }
-      })
+	//Клик на карту - карта активна, клик куда угодно, но не на карту - карта не активна
+	let map = document.querySelector('#map-wrap iframe');
+	document.addEventListener('click', function(e) {
+		if(e.target.id === 'map-wrap') {
+			map.style.pointerEvents = 'all';
+		} else {
+			map.style.pointerEvents = 'none';
+		}
+	});
 
-    //pageup and  
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 800) {
-          $('.pageup').fadeIn();
-      } else {
-          $('.pageup').fadeOut();
-      }
-      
-  });
-  //Smooth scroll 
-  $("a[href^='#up']").click(function(){
-      var _href = $(this).attr("href");
-      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-      return false;
-  });
+	//pageup and  
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 800) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+	//Smooth scroll 
+	$("a[href^='#up']").click(function(){
+		const _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
+	
+	// let goTop = document.querySelector('.pageup');
+
+	// goTop.addEventListener('click', function() {
+	// 	window.scrollTo({
+	// 		top: 0,
+	// 		behavior: "smooth"
+	// 	});
+
+
 });
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ gulp.task('server', function() {
     });
 
     gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("src/js/**/*.js").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
@@ -31,6 +32,7 @@ gulp.task('styles', function() {
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
 });
 
 gulp.task('html', function () {
